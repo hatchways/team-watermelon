4446dd73ac03d5274f6733ce9b3b77c05b09fe20
chianhuei <chianhuei.wang@gmail.com>
2020-07-02 16:19:46 -0700

    cleaned Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,21 +2,20 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     
     footer: {
-      borderTop: `1px solid ${theme.palette.divider}`,
-      marginTop: theme.spacing(8),
-      paddingTop: theme.spacing(3),
-      paddingBottom: theme.spacing(3),
-      [theme.breakpoints.up('sm')]: {
-        paddingTop: theme.spacing(6),
-        paddingBottom: theme.spacing(6),
-      },
+        borderTop: `1px solid ${theme.palette.divider}`,
+        marginTop: theme.spacing(8),
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
+        [theme.breakpoints.up('sm')]: {
+            paddingTop: theme.spacing(6),
+            paddingBottom: theme.spacing(6),
+        },
     }
   }));
 
@@ -25,34 +24,14 @@ function Copyright() {
     return (
       <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
-        <Link color="inherit" href="https://material-ui.com/">
-          BigDeal
+        <Link color="inherit" href="#">
+            BigDeal
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
       </Typography>
     );
-  }
-
-const footers = [
-{
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
-},
-{
-    title: 'Features',
-    description: ['Cool feature', 'Team feature'],
-},
-{
-    title: 'Resources',
-    description: ['Resource', 'Another resource', 'Final resource'],
-},
-{
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
-},
-];
-
+}
 
 
 const Footer = ()=>{
@@ -60,31 +39,11 @@ const Footer = ()=>{
 
     return(
         <Container maxWidth="md" component="footer" className={classes.footer}>
-        <Grid container spacing={4} justify="space-evenly">
-          {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}>
-              <Typography variant="h6" color="textPrimary" gutterBottom>
-                {footer.title}
-              </Typography>
-              <ul>
-                {footer.description.map((item) => (
-                  <li key={item}>
-                    <Link href="#" variant="subtitle1" color="textSecondary">
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Grid>
-          ))}
-        </Grid>
-        <Box mt={5}>
-          <Copyright />
-        </Box>
-      </Container>
+            <Box mt={5}>
+                <Copyright />
+            </Box>
+        </Container>
     )
-
-
 }
 
 
