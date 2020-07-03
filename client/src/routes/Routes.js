106334793f7ad0a97bd6239cb93157b1db8bac82
chianhuei <chianhuei.wang@gmail.com>
2020-07-03 00:44:12 -0700

    fixed error
    fixed error

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,6 +1,6 @@
 import React,{Component} from "react";
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import LandingPage from "../pages/LandingPage";
+import LandingPage from "../pages/Landingpage";
 import MainPage from "../pages/MainPage";
 
 class Routes extends Component{
