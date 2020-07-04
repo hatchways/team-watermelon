ad3f08d34e6493daa3bbca579e880d7ef646699d
chianhuei <chianhuei.wang@gmail.com>
2020-07-04 12:55:06 -0700

    changed Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter} from "react-router-dom";
-
 import { theme } from "./themes/theme";
 import Routes from './routes/Routes';
 
 import "./App.css";
 
 function App() {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes/>
-      </BrowserRouter>
-    </MuiThemeProvider>
-  );
+    return (
+        <MuiThemeProvider theme={theme}>
+            <Routes/>
+        </MuiThemeProvider>
+    );
 }
 
 export default App;
