b1a72017483cfa9737dde74e1c39399648bd52cc
Eric Thorfinnson <ethorf@gmail.com>
2020-07-09 20:06:15 -0400

    this should be working now?

--- server/utils/scrapingFunction.js
+++ server/utils/scrapingFunction.js
@@ -3,6 +3,7 @@ const arr = [];
 // let obj = {};
 const amazonUrl =
 	'https://www.amazon.ca/Namco-Bandai-Dark-Souls-Fades/dp/B06XTJMF4B/ref=sr_1_1?dchild=1&keywords=dark+souls+4&qid=1594306190&sr=8-1';
+
 const scraping = async (url) => {
 	try {
 		const browser = await puppeteer.launch({ headless: true });
@@ -20,22 +21,8 @@ const scraping = async (url) => {
 	console.log(pageData);
 	return pageData;
 };
-const promiseTest = async () => {
-	try {
-		const bob = 'bob';
-		return bob;
-	} catch (error) {}
-};
-const promiseReturn = () => {
-	return promiseTest();
-};
-async function run() {
-	const data = await scraping(amazonUrl).then((result) => {
-		return result;
-	});
-	return data;
-	// Your logic goes here.
-}
+
 // console.log(Promise.resolve(scraping(amazonUrl)));
-console.log(scraping(amazonUrl));
+// console.log(scraping(amazonUrl));
 // console.log(run());
+module.exports = scraping;
